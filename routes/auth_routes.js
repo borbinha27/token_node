e108a64555087258f9a6e6135222a87bfa752ed0
Arthur <arthurborba271@gmail.com>
2025-08-25 11:45:52 -0300

    login

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -20,4 +20,18 @@ router.post("/register", async (req, res) =>{
     res.json({message:"Usuário registrado com sucesso"})
 })
 
+//rota de login
+router.post("/login", async (req, res) =>{
+    const {username, password} = req.body;
+
+    const userExist = users.find((u) => u.username === username);
+    if(!users) return res.status(400).json({message:"Usuário não encontrado"});
+    
+    const isPasswordValid = await bcrypt.compare(password, userExist.password);
+    if(!isPasswordValid) return res.status(400).json({message:"Senha inválida"}) 
+
+    const token = jwt.sign((username) , 'secreta123', {expiresIn:'1h'})
+    res.json({token})
+})
+
 export default router;
